735cc4fd08c2ed5a7f0b4fc5a7fb0e825eef07d8
Th-Fauconnier <fauconthierry@outlook.fr>
2021-02-04 13:22:11 +0100

    form update project

--- src/_routes/_project/UpdateProject.jsx
+++ src/_routes/_project/UpdateProject.jsx
@@ -1,16 +1,31 @@
 
-import React, { useEffect, useState } from "react";
+import React, {useContext, useEffect, useState} from "react";
 
 import Field from '../../_components/forms/Field';
 import { withTranslation } from 'react-i18next';
 import projectAPI from "../../_services/projectAPI";
+import AuthAPI from "../../_services/authAPI";
+import AuthContext from "../../_contexts/AuthContext";
+import {Button, Form, Item} from "semantic-ui-react";
 
 const UpdateProject = (props) => {
+    AuthAPI.setup();
+    const isAuthenticated = useContext(AuthContext);
+    if (isAuthenticated === true) {
+        props.history.replace('/');
+    }
 
     const id = props.match.params.id
-    console.log(id)
+ //   console.log(id)
 
     const [project, setProject] = useState({
+        title: "",
+        description: "",
+        startDate: {},
+        endDate: {},
+    });
+
+    const [errors, setErrors] = useState({
         title: "",
         description: "",
         startDate: "",
@@ -27,9 +42,7 @@ const UpdateProject = (props) => {
     }, []);
 
     const handleChange = (event) => {
-        const value = event.currentTarget.value;
-        const name = event.currentTarget.name;
-
+        const { name, value } = event.currentTarget;
         setProject({ ...project, [name]: value });
     };
 
@@ -38,54 +51,77 @@ const UpdateProject = (props) => {
 
         projectAPI.put(project)
             .then(response =>
+                //confirmation
                 console.log(response.data[0])
             )
             .catch(error => {
                 console.log(error.response)
-                setError("Echec, veuillez vérifier vos informations");
+                setErrors(error.response.data.error);
             })
     };
 
-    const [error, setError] = useState("");
+  //  const [error, setError] = useState("");
 
     return (
-        <div className="App">
-            <h1>Mise à jour du projet</h1>
-            <form onSubmit={handleSubmit}>
-                <Field
-                    label="Title"
-                    name="title"
-                    value={project.title}
-                    onChange={handleChange}
-                    placeholder="nom..."
-                />
-                <Field
-                    label="Description"
-                    name="description"
-                    type="textarea"
-                    value={project.description}
-                    onChange={handleChange}
-                    placeholder="description..."
-                />
-                <Field
-                    label="Date de début"
-                    name="startDate"
-                    type="date"
-                    value={project.startDate}
-                    onChange={handleChange}
-                />
-                <Field
-                    label="Date de fin"
-                    name="endDate"
-                    type="date"
-                    value={project.endDate}
-                    onChange={handleChange}
-                />
-                <div className="inline-btn">
-                    <button type="submit" className="btn btn-success">Enregistrer</button>
-                </div>
-                <p>{error}</p>
-            </form>
+        <div className="card">
+            {project && (
+                <>
+                    <Item.Group >
+                        <Item>
+                            <Item.Content>
+                                <Form onSubmit={handleSubmit}>
+                                    <Form.Input
+                                        icon=''
+                                        iconPosition='left'
+
+                                        label="Title"
+                                        name="title"
+                                        value={project.title}
+                                        onChange={handleChange}
+                                        placeholder="nom..."
+                                        error={errors.name ? errors.name : null}
+                                    />
+                                    <Form.Input
+                                        icon=''
+                                        iconPosition='left'
+
+                                        label="Description"
+                                        name="description"
+                                        type="textarea"
+                                        value={project.description}
+                                        onChange={handleChange}
+                                        placeholder="description..."
+                                        error={errors.name ? errors.name : null}
+                                    />
+                                    <Form.Input
+                                        icon=''
+                                        iconPosition='left'
+
+                                        label="Date de début"
+                                        name="startDate"
+                                        type="date"
+                                        value={project.startDate}
+                                        onChange={handleChange}
+                                        error={errors.name ? errors.name : null}
+                                    />
+                                    <Form.Input
+                                        icon=''
+                                        iconPosition='left'
+
+                                        label="Date de fin"
+                                        name="endDate"
+                                        type="date"
+                                        value={project.endDate}
+                                        onChange={handleChange}
+                                        error={errors.name ? errors.name : null}
+                                    />
+                                    <Button content='Valider' primary />
+                                </Form>
+                            </Item.Content>
+                        </Item>
+                    </Item.Group>
+                </>
+            )}
         </div>
     );
 };
