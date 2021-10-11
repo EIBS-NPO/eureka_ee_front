721294e6ccab9a957b284686706551031db0d457
Th-Fauconnier <fauconthierry@outlook.fr>
2021-10-11 11:16:14 +0200

    debug errors handler for registerForm

--- src/__appComponents/_routerComponents/_pages/RegisterPage.jsx
+++ src/__appComponents/_routerComponents/_pages/RegisterPage.jsx
@@ -25,14 +25,7 @@ const RegisterPage = ({ history }) => {
         passwordConfirm: "",
     });
 
-    const [errors, setErrors] = useState({
-        picture: undefined,
-        email: undefined,
-        lastname: undefined,
-        firstname: undefined,
-        password: undefined,
-        passwordConfirm: undefined,
-    });
+    const [errors, setErrors] = useState({});
 
     const handleChange = (event) => {
         const { name, value } = event.currentTarget;
@@ -41,36 +34,51 @@ const RegisterPage = ({ history }) => {
 
     const checkFormValidity = (user) => {
         let errorsRslt = []
+        let boolRslt = true;
+
         //checkPassword
         if(!checkPassword(user.password)){
-            errorsRslt["password"] = t("password_bad_pattern")
+            console.log("password_bad_pattern")
+            errorsRslt["password"] = "password_bad_pattern"
+            boolRslt = false;
         }else errorsRslt["password"] = undefined
 
         if(!(user.password === user.passwordConfirm) || user.passwordConfirm === "") {
-            errorsRslt["passwordConfirm"] = t("password_not_match")
+            console.log("password_not_match")
+            errorsRslt["passwordConfirm"] = "password_not_match"
+            boolRslt = false;
         }else errorsRslt["passwordConfirm"] = undefined
 
         //lastname
         if (!checkStringLenght(user.lastname, 2, 50)){
-            errorsRslt["lastname"] = t("error_namePattern")
+            console.log("error_namePattern")
+            errorsRslt["lastname"] = "error_namePattern"
+            boolRslt = false;
         }else  errorsRslt["lastname"] = undefined
 
         //firstname
         if(!checkStringLenght(user.firstname, 2, 50)) {
-            errorsRslt["firstname"] = t("error_namePattern")
+            console.log("error_namePattern")
+            errorsRslt["firstname"] = "error_namePattern"
+            boolRslt = false;
         }else errorsRslt["firstname"] = undefined
 
-        return errorsRslt
+        console.log(errorsRslt)
+        console.log(errorsRslt.length === 0 )
+        if(boolRslt){
+            setErrors({})
+            return boolRslt
+        }else {
+            setErrors(errorsRslt)
+            return boolRslt;
+        }
     }
 
     const [loader, setLoader] = useState(false)
     const handleSubmit = () => {
-        let formErrors = checkFormValidity(user)
-        console.log(formErrors)
-        console.log(formErrors.length)
-        if ( formErrors.length === 0){
+        let isFormValid = checkFormValidity(user)
+        if (isFormValid) {
             setLoader(true)
-console.log(loader)
             UserAPI.register(user)
                 .then(response => {
                     mailerAPI.sendConfirmMail(t, response.data[0])
@@ -84,18 +92,19 @@ console.log(loader)
                 .catch(error => {
                     console.log(error.response)
                     setLoader(false)
-                    //    setErrors(error.response)
+                    setErrors(error.response.data)
                 })
-                .finally(()=>setLoader(false))
-        }else{
+                .finally(() => setLoader(false))
+        }/*else{
+            console.log(errors)
             setErrors(formErrors)
-        }
+        }*/
     };
 
     return (
         <Segment basic className="card">
             {loader &&
-                 <Loader active>{t('activation_of_your_account')}</Loader>
+                 <Loader active>{t('create_your_account')}</Loader>
             }
             {!loader &&
                 <Form onSubmit={handleSubmit} loading={loader}>
@@ -109,7 +118,7 @@ console.log(loader)
                         value={user.email}
                         onChange={handleChange}
                         placeholder={t('email')+"..."}
-                        error={errors.email ? errors.email : null}
+                        error={errors.email ? t(errors.email) : null}
                     />
                     <Form.Input
                         icon='user'
@@ -121,7 +130,7 @@ console.log(loader)
                         value={user.firstname}
                         onChange={handleChange}
                         placeholder={t('firstname')+"..."}
-                        error={errors.firstname ? errors.firstname : null}
+                        error={errors.firstname ? t(errors.firstname) : null}
                     />
                     <Form.Input
                         icon='user'
@@ -133,7 +142,7 @@ console.log(loader)
                         value={user.lastname}
                         onChange={handleChange}
                         placeholder={t('lastname')+"..."}
-                        error={errors.lastname ? errors.lastname : null}
+                        error={errors.lastname ? t(errors.lastname) : null}
                     />
 
                     <Form.Input
@@ -146,7 +155,7 @@ console.log(loader)
                         type='password'
                         onChange={handleChange}
                         placeholder={t('password')+"..."}
-                        error={errors.password ? errors.password : null}
+                        error={errors.password ? t(errors.password) : null}
                     />
 
                     <Form.Input
@@ -159,7 +168,7 @@ console.log(loader)
                         value={user.passwordConfirm}
                         onChange={handleChange}
                         placeholder={t('passwordConfirm')+"..."}
-                        error={errors.passwordConfirm ? errors.passwordConfirm :null}
+                        error={errors.passwordConfirm ? t(errors.passwordConfirm) :null}
                     />
                     <Button className="ui primary basic button" content= { t('Sign_up') } />
                 </Form>
